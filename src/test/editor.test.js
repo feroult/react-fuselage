d4cab94bdf66658cf76f399ca7c8909135267bad
feroult <feroult@gmail.com>
2017-11-29 09:54:42 -0200

    test setup

--- src/test/editor.test.js
+++ src/test/editor.test.js
@@ -2,15 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Editor} from '../lib/editor';
 
-const TestEditor = (props) => {
-    return (
-        <Editor value={{}}>
-            <div>hello</div>
-        </Editor>
-    );
-};
+import './helper';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<TestEditor/>, div);
+describe('Editor', () => {
+    it('renders without crashing', () => {
+        const TestEditor = (props) =>
+            <Editor value={{}}>
+                <div>hello</div>
+            </Editor>;
+
+        const div = document.createElement('div');
+        ReactDOM.render(<TestEditor/>, div);
+    });
+
+    it('contains inner children', () => {
+        const TestEditor = (props) =>
+            <Editor value={{}}>
+                <div>hello</div>
+            </Editor>;
+
+        const editor = shallow(<TestEditor/>);
+        expect(editor.find('div').length).to.be.equal(1);
+    });
 });
