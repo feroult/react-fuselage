b725b4076298bbad3e990df415bcffa0c4ffdf82
feroult <feroult@gmail.com>
2017-11-29 22:28:44 -0200

    lint

--- src/lib/editor.js
+++ src/lib/editor.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Handler} from './handler/handler';
 import {GridEditor} from './grid-editor';
