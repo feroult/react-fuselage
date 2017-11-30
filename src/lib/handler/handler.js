1462671aed792d5ccbd00e691503aa067b7ac196
feroult <feroult@gmail.com>
2017-11-29 22:59:58 -0200

    undo redo

--- src/lib/handler/handler.js
+++ src/lib/handler/handler.js
@@ -1,13 +1,22 @@
+import * as mobx from 'mobx';
+
 import StateHandler from './state-handler';
 import KeyboardHandler from './keyboard-handler';
 import UndoRedoHandler from './undo-redo-handler';
 
 class Handler {
     constructor(value) {
-        this._value = value;
+        this.observe(value);
         this.stateHandler = new StateHandler(this);
         this.undoRedoHandler = new UndoRedoHandler(this);
         this.keyboardHandler = new KeyboardHandler(this);
+    }
+
+    observe(value) {
+        this._value = value;
+        if (!mobx.isObservable(value)) {
+            mobx.extendObservable(value, value);
+        }
     }
 
     get value() {
@@ -17,6 +26,7 @@ class Handler {
     set value(value) {
         this._value = value;
     }
+
 }
 
 
--- src/lib/handler/state-handler.js
+++ src/lib/handler/state-handler.js
@@ -1,16 +1,6 @@
-import * as mobx from 'mobx';
-
 class StateHandler {
     constructor(handler) {
         this.handler = handler;
-        this.observe();
-    }
-
-    observe() {
-        const value = this.handler.value;
-        if (!mobx.isObservable(value)) {
-            mobx.extendObservable(value, value);
-        }
     }
 }
 
