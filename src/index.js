7e45b84204744f1cf45cc5e0b9abd306e0dc29cd
Fernando Ultremare <feroult@gmail.com>
2020-03-23 00:07:55 -0300

    dev lib

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ export { Tab, TabGroup } from './components/editor/Tab';
 export { Login } from './components/login/Login';
 export { Fuselage, Page } from './components/fuselage/Fuselage';
 export { SessionStore } from './stores/firebase-session-store';
+export { connect }from './utils/middleware';
 
